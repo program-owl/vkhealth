268cacfc68d44a86b72d590bab5aa94f8722b02c
Daniil Chizhevskij <daniilchizhevskij@gmail.com>
2019-05-01 22:14:20 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'vkhealth',
+  packages = ['vkhealth'],
+  version = '0.1',
+  description = 'Module that shows the health of VKontakte platform.',
+  author = 'Daniil Chizhevskij',
+  author_email = 'daniilchizhevskij@gmail.com',
+  url = 'https://github.com/DaniilChizhevskii/vkhealth',
+  download_url = 'https://github.com/DaniilChizhevskii/vkhealth/tarball/0.1',
+  keywords = ['vk', 'vkontakte', 'platform', 'health'],
+  classifiers = [],
+)
